b2686c00da3cc17c845ea1512e3b46374c022f74
shuntowski <Samuel.Huntowski@gmail.com>
2022-08-02 23:53:32 -0500

    Correct several errors with Maven and build paths

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # evive_tech_test_1
 This is the Evive Engineering Test provided to me on 7/29/2022
+
---
+++ evive_tech/src/main/java/com/evive_technical/menuSystem.java
@@ -0,0 +1,26 @@
+package com.evive_technical;
+
+public class menuSystem 
+{
+    public static void main( String[] args )
+    {
+        /* Menu Ordering System 
+         * 1) Take input from user
+         *  1.1) take input
+         *  1.2) flatten text so that it's uniform
+         *  1.3) check for invalid input symbols (i.e. only letters and numbers)
+         * 2) Parse via commas
+         *  2.1) parse via commas
+         *  2.2) check for particular meal based on first item
+         *  2.3) check that all other arguments are numbers
+         * 3) Call meal request based on the numbers given
+         *  3.1) call meal request and pass in order numbers
+         *  3.2) reject order based on issues with order numbers and special rules (too large of a number, 
+         *      too many items, etc)
+         *  3.3) assemble meal request and return
+         * 4) Output meal request
+        */
+
+        
+    }
+}
